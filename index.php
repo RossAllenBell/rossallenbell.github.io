6b3c85137c10cd234a57ce39094d492f085355ea
Ross Bell <rossallenbell@gmail.com>
2012-04-10 20:04:06 -0500

    Refactor and remove tables

--- index.php
+++ index.php
@@ -2,7 +2,19 @@
 	include("include.php");
 	include($rootDirectory . "/include/header.php");
 ?>
-Welcome to RossAllenBell.com! Because it's hard to guess, my name is Ross Allen Bell. There's not a whole lot here at the moment. Some day, when I become ambitious, I'll go ahead and create some real content. For now, I'll simply grace you with some god-awful coloring schemes and a resume that I'm sure I'll forget to update. Thanks for stopping by!
-<p/>
-Oh, and if you need to contact me, drop me a line at RossAllenBell at gmail.
+<p>
+My name is Ross Allen Bell. I'm married to <a href="http://www.shatra.net/" target="_blank">Shatra Bell</a>, have a too-cool-for-me sister Alison, and parents Rick and Kathy. Shatra and I met while on the <a href="http://www.nasa.gov/audience/forstudents/postsecondary/programs/Undergraduate_Student_Research_Project.html" target="_blank">USRP<a/> at <a href="http://www.nasa.gov" target="_blank">NASA</a> <a href="http://www.nasa.gov/centers/langley/home/index.html" target="_blank">Langley</a>.
+</p>
+<p>
+I was born in Houston, Texas on 1985-08-10. I moved with my family at the age of about ten to Omaha, Nebraska. I finished my undergraduate degree at the <a href="http://www.pki.nebraska.edu/" target="_blank">PKI</a> of <a href="http://www.unomaha.edu" target="_blank">University of Nebraska at Omaha</a> in Computer Science.
+</p>
+<p>
+Currently, I'm working at Backstop Solutions in Chicago, and I can definitely say I've found a great place to work with an exceptional team. I love living downtown and haven't driven a car since I lived in Texas. I'd be hard pressed to ever consider living in a "suburb" type situation again.
+</p>
+<p>
+My interests include <a href="http://www.java.com" target="_blank">Java</a>, artificial intelligence, and entertainment media. I'm a huge movie fan, and when I'm not too busy, I like to wake up by going to the gym.
+</p>
+<p>
+If you need to contact me, drop me a line at RossAllenBell at gmail.
+</p>
 <?php include($rootDirectory . "/include/footer.php"); ?>
